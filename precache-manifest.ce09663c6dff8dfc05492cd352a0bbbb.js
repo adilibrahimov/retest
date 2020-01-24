1abbf2149cebf48aea86da82d42374e581c37581
adil ibrahimov <adil.ibrahimov.2012@mail.ru>
2020-01-24 19:31:12 +0400

    wer

---
+++ precache-manifest.ce09663c6dff8dfc05492cd352a0bbbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93743521cc78d929e019a3e1e6e7d8a",
+    "url": "/retest/index.html"
+  },
+  {
+    "revision": "ecf978cea992c684d4a0",
+    "url": "/retest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd07b41fd8b200aa0983",
+    "url": "/retest/static/js/2.ed930f27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/retest/static/js/2.ed930f27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecf978cea992c684d4a0",
+    "url": "/retest/static/js/main.c7d5dc8c.chunk.js"
+  },
+  {
+    "revision": "e35ec45c02ce29c6feba",
+    "url": "/retest/static/js/runtime-main.1a2b493a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/retest/static/media/logo.5d5d9eef.svg"
+  }
+]);
